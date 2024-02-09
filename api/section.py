45c298cb98aa59d8cbffaf32dd942f204b3b159d
Jolina1205 <minezesjolina@gmail.com>
2024-02-09 15:21:50 +0530

    section 2 complete

---
+++ api/section.py
@@ -0,0 +1,16 @@
+import fastapi
+
+router= fastapi.APIRouter()
+
+@router.get("/section/{id}")
+async def read_section():
+    return {"courses":[]}
+
+@router.get("/section/{id}/content-blocks")
+async def read_section_content_blocks():
+    return {"courses":[]}
+
+@router.get("/content-blocks/{id}")
+async def read_content_block():
+    return {"courses":[]}
+
